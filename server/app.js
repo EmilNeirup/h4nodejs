d2d8170d37f2a4fe860dee59ebcbe0bfc05a7dc0
Emil Neirup Jensen <emilnjensen@gmail.com>
2021-02-23 14:55:54 +0100

    added db connection

--- server/app.js
+++ server/app.js
@@ -1,14 +1,30 @@
 var http = require('http');
 var fs = require('fs');
+const sql = require('mssql')
 
-http.createServer((req, res) => {
-    var path = req.url;
+async function getRooms() {
+    try {
+        await sql.connect('mssql://sa:Pass1234@localhost/servprogsolutionsRest')
+        const result = await sql.query`select * from dbo.rooms`
+        return result.recordset
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+http.createServer(async (req, res) => {
+    var rooms = await getRooms()
+    console.log(rooms)
+
+    var path = req.url.split('?')[0];
+
     if (path === '/') { 
         path = 'index.html' 
     }
     else {
-        path = path.substring(1);
+        path = req.url.substring(1);
     };
+
 
     fs.readFile(path, (err, data) => {
 
@@ -16,8 +32,7 @@ http.createServer((req, res) => {
             console.log(err)
             data = '404 data not found';
         }
-
-        res.write(data);
-        return res.end();
+        res.write(data)
+        return res.end()
   });
 }).listen(8080);
--- server/script.js
+++ server/script.js
@@ -1,3 +1,8 @@
-function warning() {
-    alert("Dette er en advarsel!")
+function changeText() {
+    var name =  new URLSearchParams(window.location.search).get('name') ?? '';
+    document.getElementById("dispaly-text").innerHTML = 'Your name: ' + name;
+}
+
+window.onload = () => {
+    changeText()
 }
